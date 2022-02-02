1f045493d4ad3c9a55588c466e05f465c1ce08be
unknown <santicorocottax@gmail.com>
2022-02-02 12:36:31 +0100

    version 0.0.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import './App.scss';
 function App() {
   return (
     <div>
-      <p>hola putos</p>
-      <p>GOT is comming</p>
-      <p>GOT is comming 2</p>
-      <p> Se escribe God cabrones, the Godess is here</p>
+      
     </div>
   );
 }
