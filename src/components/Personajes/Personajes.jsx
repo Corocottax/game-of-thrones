05086be50f30dd0adbc18f7d49f2b54d7f05c736
Hugo Perez Garcia <hugoperez80@hotmail.com>
2022-02-02 20:09:29 +0100

    trabajando en mi componente

--- src/components/Personajes/Personajes.jsx
+++ src/components/Personajes/Personajes.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import "./Personajes.scss"
-import Navbar from "../Inicio/Navbar/Navbar"
-import Idiomas from '../../shared/Idiomas/Idiomas';
-import Castillito from "../../shared/Castillito/Castillito"
+import React from "react";
+import "./Personajes.scss";
+import Navbar from "../Inicio/Navbar/Navbar";
+import Idiomas from "../../shared/Idiomas/Idiomas";
+import Castillito from "../../shared/Castillito/Castillito";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { generatePath } from "react-router-dom";
 
 const Personajes = () => {
-    return (
-        <div>
+  const [characters, setCharacters] = useState([]);
 
-            <div className='inicio-header'>
+  useEffect(() => {
+    fetch("https://api.got.show/api/show/characters/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((characters) => {
+        setCharacters(characters);
+      });
+  }, []);
 
-                <Castillito />
-                <Idiomas />
+  return (
+    <div>
+      <div className="inicio-header">
+        <Castillito />
+        <Idiomas />
+      </div>
 
-            </div>
-
-            <h1>Personajes works!</h1>
-
-            <Navbar />
-
-        </div>
-    );
+      <Navbar />
+      {characters.map((character) => {
+        return (
+            <Link className="enlaceCharactersDetail" to={generatePath("/personajes/:name", { name: character.name })} >
+          <div className="cartita">
+            <img src={character.image} alt={character.name} />
+            <h1 className="nombre">{character.name}</h1>
+          </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Personajes;
