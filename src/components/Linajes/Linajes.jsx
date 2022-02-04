f0bd5e840ff99176a0f31025fbbf92ab7069f5f7
Alba <alba.m.navarro@gmail.com>
2022-02-04 20:09:28 +0100

    updated

--- src/components/Linajes/Linajes.jsx
+++ src/components/Linajes/Linajes.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState,} from "react";
-import {Link} from "react-router-dom";
 import "./Linajes.scss";
 import Navbar from "../Inicio/Navbar/Navbar";
 import Castillito from "../../shared/Castillito/Castillito";
 import Idiomas from "../../shared/Idiomas/Idiomas";
 import LinajesGallery from "../LinajesGallery/LinajesGallery";
 import { getHouses } from "../../functions/Functions";
+import Buscador from "../../shared/Buscador/Buscador"
 
 export default function Linajes() {
   const [Casas, setCasas] = useState([]);  
@@ -23,7 +23,7 @@ export default function Linajes() {
   return (
     <>
       <div className="divHead">
-          <Link to="/Inicio">🡰Volver</Link>
+       <Buscador />
         <figcaption className="casLen" >
           <Castillito />
           <Idiomas />
