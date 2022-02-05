26911119c8e82b5cb31fdefaf9a3a39743217677
unknown <santicorocottax@gmail.com>
2022-02-05 16:41:39 +0100

    fixeo las banderitas

--- src/shared/Idiomas/Idiomas.jsx
+++ src/shared/Idiomas/Idiomas.jsx
@@ -8,16 +8,8 @@ const Idiomas = () => {
   return (
   <div className='idiomitas'>
 
-    <button className='banderitaEs' type='button'  value={"es"} onClick={context.selectLanguage}>
-
-      <img className='banderita' src='https://res.cloudinary.com/dhp2zuftj/image/upload/v1643819219/spain_2_nibk1z.png' alt='españita' />
-
-    </button>
-    <button className='banderitaEn' type='button' value={"en"} onClick={context.selectLanguage}>
-
-      <img className='banderita' src='https://res.cloudinary.com/dhp2zuftj/image/upload/v1643819251/united-kingdom_1_x47jxq.png' alt='englandcita' />
-
-    </button>
+    <button className='banderitaEs' type='button'  value={"es"} onClick={context.selectLanguage}></button>
+    <button className='banderitaEn' type='button' value={"en"} onClick={context.selectLanguage}></button>
 
   </div>
   );
