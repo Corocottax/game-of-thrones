e5de1eb83ec6a8dc3f42d2110427da86a1e57cdb
Alba <alba.m.navarro@gmail.com>
2022-02-05 14:09:29 +0100

    con idiomas y errores

--- src/components/Inicio/Navbar/Navbar.jsx
+++ src/components/Inicio/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "./Navbar.scss"
+import { FormattedMessage } from "react-intl";
 
 const Footer = () => {
     return (
         <nav>
 
-            <NavLink to="/personajes" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>Personajes</NavLink>
-            <NavLink to="/linajes" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>Casas</NavLink>
-            <NavLink to="/cronologia" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>Cronologia</NavLink>
+            <NavLink to="/personajes" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+            <FormattedMessage id="Charac" defaultMessage="Personajes"/>
+            </NavLink>
+
+            <NavLink to="/linajes" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+            <FormattedMessage id="Houses" defaultMessage="Casas"/>
+            </NavLink>
+
+            <NavLink to="/cronologia" activeclassname={"active"} className={({isActive}) => (isActive ? "active" : "inactive")}>
+            <FormattedMessage id="Crono" defaultMessage="Cronologia"/>
+            </NavLink>
 
         </nav>
     );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Wrapper from './shared/Wrapper/Wrapper';
 
 ReactDOM.render(
   <React.StrictMode>
+  <Wrapper>
     <App />
+    </Wrapper>
   </React.StrictMode>,
   document.getElementById('root')
 );
